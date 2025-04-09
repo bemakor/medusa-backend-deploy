8fee21c4b6273135bc8aa15382649fad433d79de
RameshXt <rameshxt.1@gmail.com>
2025-04-09 15:05:25 +0530

    Feat: force deploy ecs task

--- image-updater.sh
+++ image-updater.sh
@@ -2,8 +2,16 @@
 
 IMAGE_TAG=$1
 
-# Find and replace the image tag in the ecs.tf file
+# IMAGE UPDATER
 echo "Updating ecs.tf with the new image tag: $IMAGE_TAG"
 sed -i "s|image\s*=\s*\".*\"|image = \"$IMAGE_TAG\"|" terraform/ecs.tf
 
 echo "ecs.tf file updated successfully with image tag: $IMAGE_TAG"
+
+# FORCE DEPLOYMENT NEW TASK
+echo "Forcing a new deployment of the ECS service..."
+aws ecs update-service \
+  --cluster medusa-cluster \
+  --service medusa-service \
+  --force-new-deployment
+echo "ECS service updated successfully."
